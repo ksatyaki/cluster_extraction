fbefe5c6bf81cc6a06b13906b063cf15e50548fa
Chittaranjan Srinivas <chitt@live.in>
2014-10-16 17:34:05 +0200

    max plane iterations

--- src/cluster_extraction.cpp
+++ src/cluster_extraction.cpp
@@ -178,7 +178,7 @@ void ClusterExtraction::processCloud(float plane_tolerance)
 	seg.setOptimizeCoefficients (true);
 	seg.setModelType (pcl::SACMODEL_PLANE);
 	seg.setMethodType (pcl::SAC_RANSAC);
-	seg.setMaxIterations (100);
+	seg.setMaxIterations (1000);
 	seg.setDistanceThreshold (0.02);
 
 	// Remove the planes.
